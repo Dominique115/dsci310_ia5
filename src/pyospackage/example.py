e64df75cd5304e6aff4ea29a60c88b13dc9fa0dc
Dominique115 <jingzhichen030620@gmail.com>
2026-03-31 18:19:52 -0700

    created pyospackage_dominique115

---
+++ src/pyospackage/example.py
@@ -0,0 +1,65 @@
+"""
+A module that adds and multiplies numbers together.
+
+You may want to delete this module or modify it for your package.
+It's generally good practice to have a docstring
+that explains the purpose of the module, at the top.
+"""
+
+def add_numbers(a, b):
+    """
+    Add two numbers together and return the result.
+
+    This is an example function with a numpy style docstring.
+    We recommend using this style for consistency and readability.
+
+    Parameters
+    ----------
+    a : float
+        The first number to add.
+    b : float
+        The second number to add.
+
+    Returns
+    -------
+    float
+        The sum of the two numbers.
+
+    Examples
+    --------
+    >>> add_numbers(3, 5)
+    8
+    >>> add_numbers(-2, 7)
+    5
+
+    """
+    return a + b
+
+def mult_numbers(a, b):
+    """
+    multiplies two numbers together and return the result.
+
+    This is an example function with a numpy style docstring.
+    We recommend using this style for consistency and readability.
+
+    Parameters
+    ----------
+    a : float
+        The first number to multiply.
+    b : float
+        The second number to multiply.
+
+    Returns
+    -------
+    float
+        The result of the multiplication.
+
+    Examples
+    --------
+    >>> mult_numbers(3, 5)
+    15
+    >>> mult_numbers(-2, 7)
+    -14
+
+    """
+    return a * b
